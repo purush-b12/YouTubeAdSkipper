883f023eb8a873763daecf2b73227e7ffca3f26c
purush-b12 <purushothamanb12@gmail.com>
2023-03-24 19:16:04 +0530

    updated for edge cases and error checks
    Signed-off-by: purush-b12 <purushothamanb12@gmail.com>

--- contentscript.js
+++ contentscript.js
@@ -1,13 +1,29 @@
+var isDebugMode = false;
 
 window.onload = () => {
     var mutationObserver = new MutationObserver(function(mutations) {
         mutations.forEach(element => {
-            if(element.target && element.target.innerText && element.target.innerText.indexOf('Skip Ad') != -1 ){
-                element.target.click();
+            try{
+                if(element.target && element.target.innerText && element.target.innerText.indexOf('Skip Ad') != -1 ){
+                    element.target.click();
+                }
+
+                if(element.target && element.target.classList){
+                    if(element.target.classList.length > 0 && element.target.classList[0] == 'video-ads'){
+                        var adButton = element.target.querySelector('.ytp-ad-overlay-close-button');
+
+                        if(adButton){
+                            adButton.click();
+                        }
+                    }
+                }
+
+            }catch(error){
+                if(isDebugMode){
+                    console.log(error);
+                }
             }
-            if(element.target.classList[0] == 'video-ads'){
-                element.target.querySelector('.ytp-ad-overlay-close-button').click();
-            }
+            
         });
     });
 
